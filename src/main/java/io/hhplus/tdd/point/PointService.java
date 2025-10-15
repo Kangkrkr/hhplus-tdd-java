205ead16fd7783afae9718d9e2cd38d9f7d3e286
강승윤 <kangkrkr@naver.com>
2025-10-15 21:56:32 +0900

    포인트 사용 이력에 대한 서비스 기능 및 테스트 코드 작성

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -4,6 +4,10 @@ import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+// "포인트" 라는 개념과 업무/책임에 있어서 하나의 서비스로 작성 하는 것이
+// 응집도 및 추후 트랜잭션 관리에도 유리할 것 같아, PointService 와 같은 단일 서비스로 작성..
 @Service
 public class PointService {
 
@@ -16,7 +20,12 @@ public class PointService {
         this.pointHistoryTable = pointHistoryTable;
     }
 
-    public UserPoint findUserPointById(long userId) {
+    public UserPoint findUserPointByUserId(long userId) {
         return userPointTable.selectById(userId);
     }
+
+    public List<PointHistory> findAllPointHistoryByUserId(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+    
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,14 +1,21 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 class PointServiceTest {
@@ -20,13 +27,13 @@ class PointServiceTest {
     @Mock
     private UserPointTable userPointTable;
     @Mock
-    private PointHistory pointHistory;
+    private PointHistoryTable pointHistoryTable;
 
     final long EMPTY_POINT = 0L;
 
     @Test
     @DisplayName("유효한 USER ID 가 입력된 경우, 정상적으로 회원의 보유 포인트가 조회 되어야 한다.")
-    public void givenUserId_whenCallingFindByUserId_thenReturnsUserPoint() {
+    public void givenExistentUserId_whenCallingFindByUserId_thenReturnsUserPoint() {
 
         final long userId = 1L;
         final long point = 500L;
@@ -35,7 +42,7 @@ class PointServiceTest {
         Mockito.when(userPointTable.selectById(userId)).thenReturn(expectedUserPoint);
 
         // 실제 pointService 테스트 객체로부터 UserPoint 조회
-        UserPoint actualUserPoint = pointService.findUserPointById(userId);
+        UserPoint actualUserPoint = pointService.findUserPointByUserId(userId);
 
         // pointService 가 userPointTable 로 부터 조회해온 객체는 null 일 수 없음을 확인
         Assertions.assertNotNull(actualUserPoint);
@@ -55,13 +62,44 @@ class PointServiceTest {
         UserPoint emptyUserPoint = UserPoint.empty(nonExistentUserId);
         Mockito.when(userPointTable.selectById(nonExistentUserId)).thenReturn(emptyUserPoint);
 
-        UserPoint actualUserPoint = pointService.findUserPointById(nonExistentUserId);
+        UserPoint actualUserPoint = pointService.findUserPointByUserId(nonExistentUserId);
 
         // 데이터 검증
         Assertions.assertNotNull(actualUserPoint);
         Assertions.assertEquals(emptyUserPoint.id(), actualUserPoint.id());
-        // 해당 회원의 포인트는 0 이어야 한다.
+        // 해당 회원 객체의 포인트는 0 이어야 한다.
         Assertions.assertEquals(EMPTY_POINT, actualUserPoint.point() );
     }
+
+    @ParameterizedTest
+    @MethodSource("providePointHistoryStubs")
+    @DisplayName("회원 ID 가 주어진 경우, 이에 맞는 PointHistory 목록을 반환 한다.")
+    public void givenExistentUserId_whenCallingFindAllPointHistoryByUserId_thenReturnsMatchingPointHistories(final List<PointHistory> pointHistoryStubs) {
+        final long userId = 1L;
+
+        Mockito.when(pointHistoryTable.selectAllByUserId(userId)).thenReturn(pointHistoryStubs);
+
+        List<PointHistory> actualPointHistories = pointService.findAllPointHistoryByUserId(userId);
+
+        // 실제 조회된 객체는 null 일 수 없으므로 not null 체크
+        Assertions.assertNotNull(actualPointHistories);
+        // 두 객체 간의 size 비교 (일치 해야함)
+        Assertions.assertEquals(pointHistoryStubs.size(), actualPointHistories.size());
+        // PointHistory가 record 타입이므로 List 전체를 아래와 같이 비교..
+        Assertions.assertEquals(pointHistoryStubs, actualPointHistories);
+    }
+
+   private static Stream<Arguments> providePointHistoryStubs() {
+       final long userId = 1L;
+       List<PointHistory> historyStubs = List.of(
+                new PointHistory(1L, userId, 1000L, TransactionType.CHARGE, System.currentTimeMillis()),
+                new PointHistory(2L, userId, 200L, TransactionType.USE, System.currentTimeMillis()),
+                new PointHistory(3L, userId, 500L, TransactionType.CHARGE, System.currentTimeMillis())
+        );
+        return Stream.of(Arguments.of(historyStubs));
+    }
+
+
+
 
 }

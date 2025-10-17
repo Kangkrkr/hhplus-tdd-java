bfd96a4177c7da934e98ef588c014ebede399a81
강승윤 <kangkrkr@naver.com>
2025-10-17 15:50:48 +0900

    유호하지 않은 사용/충전 시도 및 잔액부족에 대응하기 위한 커스텀 예외 클래스 추가

---
+++ src/main/java/io/hhplus/tdd/exception/IllegalPointException.java
@@ -0,0 +1,8 @@
+package io.hhplus.tdd.exception;
+
+public final class IllegalPointException extends RuntimeException {
+
+    public IllegalPointException(String message) {
+        super(message);
+    }
+}
